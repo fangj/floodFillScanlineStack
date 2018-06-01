60b1f9dcd1a045615084e3c6bf6f3375db178a85
Fang Jian <fangjianmail@126.com>
2018-06-01 16:18:14 +0800

    scanline flood fill ok

--- WebContent/recover.js
+++ WebContent/recover.js
@@ -139,7 +139,8 @@ function floodfill(bmd,x,y,borderColor,fillColor){
 		return colorA.r===colorB.r && colorA.g===colorB.g && colorA.b===colorB.b;
 	}
 	
-	_floodfill(x,y,borderColor,fillColor,_getPixel,_setPixel,_isColorSame,bmd.width-1,bmd.height-1);
+	//_floodfill(x,y,borderColor,fillColor,_getPixel,_setPixel,_isColorSame,bmd.width,bmd.height);
+	floodFillScanlineStack(x,y,borderColor,fillColor,_getPixel,_setPixel,_isColorSame,bmd.width,bmd.height);
 	bmd.dirty=true;
 
 }
@@ -147,6 +148,9 @@ function floodfill(bmd,x,y,borderColor,fillColor){
 function _floodfill(x,y,borderColor,fillColor,getPixel,setPixel,isColorSame,MAX_X,MAX_Y){
 	var deadCounter=10000;
 	function couldFill(x,y){
+		if(x<0||x>=MAX_X||y<0||y>=MAX_Y){
+			return false;
+		}
 		var color=getPixel(x,y);
 //		debugger
 		//console.log(x,y,color)
@@ -165,6 +169,66 @@ function _floodfill(x,y,borderColor,fillColor,getPixel,setPixel,isColorSame,MAX_
 	}
 	__floodfill(x,y);
 	setPixel(x,y,fillColor,true);//刷新显示
+}
+
+//http://lodev.org/cgtutor/floodfill.html#Scanline_Floodfill_Algorithm_With_Stack
+function floodFillScanlineStack(x,y,borderColor,fillColor,getPixel,setPixel,isColorSame,MAX_X,MAX_Y)
+{
+	  
+	  function couldFill(x,y){
+			var color=getPixel(x,y);
+			return (!isColorSame(color,borderColor)&&!isColorSame(color,fillColor));
+	  }
+	  
+  if(isColorSame(borderColor,fillColor)) return;
+
+
+  var x1;
+  var spanAbove=false, spanBelow=false;
+
+  var stack=[];
+  stack.push({x:x,y:y});
+
+  var point;
+  
+  while(point=stack.pop())
+  {
+//	  console.log(point)
+	  var x=point.x;
+	  var y=point.y;
+	  
+    x1 = x;
+    while(x1 >= 0 && couldFill(x1,y)) x1--;
+    x1++;
+    spanAbove = spanBelow = false;
+    while(x1 < MAX_X && couldFill(x1,y))
+    {
+
+      setPixel(x1,y,fillColor);
+
+      if(!spanAbove && y > 0 && couldFill(x1,y-1))
+      {
+    	 stack.push({x:x1,y:y-1});
+        spanAbove = true;
+      }
+      else if(spanAbove && y > 0 && !couldFill(x1,y-1))
+      {
+        spanAbove = false;
+      }
+      if(!spanBelow && y < MAX_Y - 1 && couldFill(x1,y+1))
+      {
+    	  stack.push({x:x1,y:y+1});
+        spanBelow = true;
+      }
+      else if(spanBelow && y < MAX_Y - 1 && !couldFill(x1,y+1))
+      {
+        spanBelow = false;
+      }
+      x1++;
+    }
+//    debugger;
+  }
+  setPixel(x,y,fillColor,true);//刷新显示
 }
 
 
